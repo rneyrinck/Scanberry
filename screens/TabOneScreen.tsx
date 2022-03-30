458549720c266022b738b981c85cda6737eb195b
robert Neyrinck <robert.a.neyrinck@gmail.com>
2022-03-30 17:55:03 -0500

    finalized functionality of dropdown(still needs styling) dropdown budget indicator will change color if over budget, set text in dropdown to state total and display over/under budget message

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -4,7 +4,7 @@ import {
   TouchableOpacity,
   Modal,
   FlatList,
-  
+  Image,
 } from "react-native";
 import React, { FC, useState, useEffect, ReactElement } from "react";
 import { BarCodeScanner } from "expo-barcode-scanner";
@@ -13,6 +13,7 @@ import { View, Text } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
 import Colors from "../constants/Colors";
 import Dropdown from "./TabTwoScreen";
+import { wrap } from "module";
 // import React, { FC, useState } from 'react';
 // import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 // import { View } from '../components/Themed';
@@ -23,12 +24,31 @@ export default function TabOneScreen({
   const [scanned, setScanned] = useState(false);
   const [text, setText] = useState("Not yet scanned");
   // usestate for current budget
-  const [budget, setBudget] = useState(15);
+  const [budget, setBudget] = useState(10);
+  // current total is added up price of all items in cart
+  const [currentTotal, setCurrentTotal] = useState(0);
   // storage for scanned item information
   const [scannedItemStorage, setScannedItemStorage] = useState({});
   // list of user saved items pulled from scan api call
-  const [productList, setProductList] = useState([]);
-
+  const [productList, setProductList] = useState([
+    {
+      barcode: "766218109223",
+      description:
+        "Whether you are starting a DIY project, creating unique gifts or upcycling various odds and ends this highly-pigmented Americana Acrylic Color is ideal for decorative painting, home decor and general craft projects. The vibrant, rich acrylic color blends easily with others in the Americana line, producing smooth, creamy colors that dry to a durable matte finish. Transform a variety of surfaces including wood, canvas, plaster, resins, papier mache, candles, walls, fabric, leather, Styrofoam, ceramic bisque, polymer clay, paper, poster board, metal and more.",
+      image: "https://images.barcodelookup.com/9614/96144763-1.jpg",
+      price: "9.99",
+      title: "DecoArt Americana Acrylic Color, 16 Oz., Slate Gray",
+    },
+    {
+      barcode: "766218109225",
+      description:
+        "Whether you are starting a DIY project, creating unique gifts or upcycling various odds and ends this highly-pigmented Americana Acrylic Color is ideal for decorative painting, home decor and general craft projects. The vibrant, rich acrylic color blends easily with others in the Americana line, producing smooth, creamy colors that dry to a durable matte finish. Transform a variety of surfaces including wood, canvas, plaster, resins, papier mache, candles, walls, fabric, leather, Styrofoam, ceramic bisque, polymer clay, paper, poster board, metal and more.",
+      image: "https://images.barcodelookup.com/9614/96144763-1.jpg",
+      price: "9.99",
+      title: "DecoArt Americana Acrylic Color, 16 Oz., Bright White",
+    },
+  ]);
+  const [budgetCardIconColor, setBudgetCardIconColor] = useState('#00ff00')
   // overlay visibility for productlist and bduget
   const [visible, setVisible] = useState(false);
   // urls for api call
@@ -38,15 +58,35 @@ export default function TabOneScreen({
   // example fetch for barcode lookup
   // https://api.barcodelookup.com/v3/products?barcode=9780140157376&formatted=y&key=krrps3snh5q72np1iektbej3l34vmb
 
-  // const productListOverlay = () => {
-  //   setVisible(true);
-  //   onclose = () => setVisible(false);
-  //   return (
-  //     <Overlay visible={visible} onClose={onclose} closeOnTouchOutside>
-  //       <Text>Model content = productList</Text>
-  //     </Overlay>
-  //   );
-  // };
+  // formula for setting current total state by mapping through items in productlist and adding their price
+  const getCurrentTotalFromProductList = () => {
+    // variable holding added price as productlist is looped over
+    let totalPlaceholder = 0;
+    // looping over productlist array and adding each items price to the placeholder value
+    for (let i = 0; i < productList.length; i++) {
+      // value is a string, as each item is looped over changed price from str into integer with decimals(parsefloat)
+      totalPlaceholder += parseFloat(productList[i].price);
+    }
+    // update current total state with value of totalPlaceholder
+    // console.log(totalPlaceholder)
+    setCurrentTotal(totalPlaceholder);
+  };
+  //function for returning text in topcard that compares current total to budget and returns over/under by x amount message
+  const currentTotalBudgetComparisonMessage = () =>{
+    if(currentTotal>budget){
+      setBudgetCardIconColor('#ff0000')
+      return(
+        `$${currentTotal - budget} over budget`
+        )
+      }else if(currentTotal<budget){
+        setBudgetCardIconColor('#00ff00')
+        return(
+          `$${budget - currentTotal} under budget`
+        )
+      }
+      console.log("get current total")
+    }
+  
 
   // setting up permissions
   const askForCameraPermission = () => {
@@ -82,10 +122,10 @@ export default function TabOneScreen({
         // const productListCopy = [...productList];
         // productListCopy.push();
       })
-      .then(() => console.log(scannedItemStorage))
+      // .then(() => console.log(scannedItemStorage))
       .catch((err) => console.log(err));
   };
-  console.log(scannedItemStorage);
+  // console.log(scannedItemStorage);
   // Add scannedItemStorage data to productList -> called when button "add to cart" in popup when item is scanned is pressed
   const addScannedItemStorageToProductList = () => {
     // copy current productlist contents as variable
@@ -94,6 +134,8 @@ export default function TabOneScreen({
     productListCopy.push(scannedItemStorage);
     // set product list state as updated list
     setProductList(productListCopy);
+    // set current total with new item added 
+    getCurrentTotalFromProductList()
     // alert that item has been added
     alert("Item added to cart!");
     // remove popup so use can continue scanning new items
@@ -141,6 +183,7 @@ export default function TabOneScreen({
       </View>
     );
   }
+  // teset data to be mapped over for
 
   // example code for dropdown picker
   // \/\/\/\/\/\/\/\/
@@ -148,52 +191,133 @@ export default function TabOneScreen({
     label: string;
   }
   const Dropdown: FC<Props> = ({ label }) => {
-    // teset data to be mapped over for
-    const ProductListDisplayInDropdown = () => {
-      productList.map((items, i) => {
-        if (productList.length === 0) {
-          console.log("length === 0")
-          return <Text>Added items display here</Text>;
-        } else {
-          return (
-            <View key={i}>
-              <Text style={{backgroundColor: 'tomato'}}></Text>
-              {/* <Text>{items.title}</Text> */}
-            </View>
-          );
-        }
-      });
-    };
-
     const [visible, setVisible] = useState(false);
 
     const toggleDropdown = () => {
       setVisible(!visible);
     };
-
+    // what is rendered when topcard with budget info is pressed -> maps over productlist and returns small cards
     const renderDropdown = () => {
       if (visible) {
-        return (<Text style={styles.dropdown}>howdy</Text>)
+        return (
+          <View style={styles.dropdown}>
+            {/* <Text>{productList[0].title}</Text> */}
+            {productList.map((item) => {
+              // console.log(item.title);
+              return (
+                // <View key={item.barcode} style={styles.dropdownItem}>
+                <View
+                  style={{
+                    backgroundColor: "#fff",
+                    width: 400,
+                    justifyContent: "center",
+                    alignContent: "center",
+                    flexGrow: 1,
+                    flex: 1,
+                    flexDirection: "row",
+                    padding: 6,
+                  }}
+                  key={item.barcode}
+                >
+                  <Image
+                    source={{ uri: item.image }}
+                    style={{ width: 50, height: 50 }}
+                  />
+                  <Text
+                    style={{
+                      color: "#000",
+                      flexShrink: 6,
+                      margin: 6,
+                      width: 230,
+                    }}
+                  >
+                    {item.title}
+                  </Text>
+                  <Button title="+" onPress={() => console.log("plus")} />
+                  <Button title="-" onPress={() => console.log("minus")} />
+                </View>
+                // </View>
+              );
+            })}
+          </View>
+        );
       }
     };
-    console.log(productList);
+    // console.log(productList[0].image);
     return (
-      <TouchableOpacity style={styles.button} onPress={toggleDropdown}>
-        {renderDropdown()}
-        <Text style={styles.buttonText}>{label}</Text>
+      // this is the top card with dropdown feature on press,
+      <TouchableOpacity
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          backgroundColor: "#ffff",
+
+          height: 80,
+          width: "90%",
+          paddingHorizontal: 10,
+          zIndex: 2,
+          borderRadius: 25,
+        }}
+        onPress={toggleDropdown}
+      >
+        {/* view holding items inside of the topcard */}
+        <View
+          style={{
+            backgroundColor: "#fff",
+            // top: 5,
+            zIndex: 2,
+            flexWrap: "wrap",
+            flexDirection: "row",
+            margin: 2,
+          }}
+        >
+          {/* topcard colored budget indicator icon */}
+          <View
+            style={{
+              // borderBottomColor: '#fff',
+              // zIndex: 8,
+              borderRadius: 20,
+              height: 60,
+              width: 60,
+              backgroundColor: budgetCardIconColor,
+              // margin: 0,
+              justifyContent: "center",
+              // alignContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {/* budget state inside of red box */}
+            <Text style={{ fontSize: 20, fontWeight: "600", color: "#000" }}>${budget}</Text>
+          </View>
+          {/* text and information view for current total and comparison to budget */}
+          <View
+            style={{
+              backgroundColor: "#ff0000",
+              marginLeft: 30,
+              flex: 1,
+              flexDirection: "column",
+            }}
+          >
+            <Text>Current total: ${currentTotal}</Text>
+            {/* function for displaying over/under budget by x amount. Takes currenttotal minus budget and returns text with amount */}
+            <Text>{currentTotalBudgetComparisonMessage()}</Text>
+          </View>
+          {/* calling function for displaying items in productlist as a dropdown */}
+          {renderDropdown()}
+        </View>
+
+        <Text style={styles.buttonText}></Text>
         {/* <Icon type='font-awesome' name='chevron-down'/> */}
       </TouchableOpacity>
     );
   };
-  // \/\/\/\/\/\/\
 
   // return the updated view after scan
   return (
     <View style={styles.container}>
-      <View>
-        <Dropdown label="ProductList" />
-        {/* <Dropdown label="Productlist" /> */}
-      </View>
+      <Dropdown label={`ProductList`} />
+      {/* <Dropdown label="Productlist" /> */}
+
       <View
         style={styles.separator}
         lightColor="#eee"
@@ -248,6 +372,8 @@ const styles = StyleSheet.create({
     overflow: "hidden",
     borderRadius: 30,
     backgroundColor: "tomato",
+    position: "relative",
+    zIndex: 1,
   },
   mainText: {
     fontSize: 60,
@@ -272,21 +398,34 @@ const styles = StyleSheet.create({
   button: {
     flexDirection: "row",
     alignItems: "center",
-    backgroundColor: "#efefef",
-    height: 50,
+    backgroundColor: "#ffff",
+
+    height: 80,
     width: "90%",
     paddingHorizontal: 10,
-    zIndex: 1,
+    zIndex: 2,
+    borderRadius: 25,
   },
   buttonText: {
     flex: 1,
     textAlign: "center",
+    color: "black",
+    zIndex: 2,
   },
   dropdown: {
     position: "absolute",
     backgroundColor: "#fff",
-    top: 50,
-    color: "#000"
+    top: 75,
+    zIndex: 2,
   },
-  
+  dropdownItem: {
+    // position: "absolute",
+    // height: 120,
+    backgroundColor: "#fff",
+    top: 2,
+    zIndex: 2,
+    flexWrap: "wrap",
+    flexDirection: "row",
+    // padding: 6,
+  },
 });
